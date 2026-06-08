551b8dfd9a955118d47b5d2a1b094af38a0612bc
Riccardo Grieco <riccardo.grieco@gbionics.ai>
2026-06-08 12:01:35 +0200

    Make the dowloaded manus sdk version configurable

--- cmake/DownloadManusSDK.cmake
+++ cmake/DownloadManusSDK.cmake
@@ -1,8 +1,8 @@
 # SPDX-FileCopyrightText: Fondazione Istituto Italiano di Tecnologia (IIT)
 # SPDX-License-Identifier: BSD-3-Clause
 
-set(_manus_sdk_version "3.1.1")
-set(_manus_sdk_archive_name "MANUS_Core_${_manus_sdk_version}_SDK.zip")
+set(MANUS_SDK_DOWNLOAD_VERSION "3.1.1" CACHE STRING "Manus SDK version to download")
+set(_manus_sdk_archive_name "MANUS_Core_${MANUS_SDK_DOWNLOAD_VERSION}_SDK.zip")
 set(_manus_sdk_url "https://static.manus-meta.com/resources/manus_core_3/sdk/${_manus_sdk_archive_name}")
 
 set(_manus_sdk_base_dir "${CMAKE_BINARY_DIR}")
@@ -10,9 +10,9 @@ set(_manus_sdk_zip_path "${_manus_sdk_base_dir}/${_manus_sdk_archive_name}")
 set(_manus_sdk_extract_dir "${_manus_sdk_base_dir}")
 
 if(UNIX)
-  set(_manus_sdk_client_dir "${_manus_sdk_extract_dir}/ManusSDK_v${_manus_sdk_version}/SDKClient_Linux")
+  set(_manus_sdk_client_dir "${_manus_sdk_extract_dir}/ManusSDK_v${MANUS_SDK_DOWNLOAD_VERSION}/SDKClient_Linux")
 else()
-  set(_manus_sdk_client_dir "${_manus_sdk_extract_dir}/ManusSDK_v${_manus_sdk_version}/SDKClient_Windows")
+  set(_manus_sdk_client_dir "${_manus_sdk_extract_dir}/ManusSDK_v${MANUS_SDK_DOWNLOAD_VERSION}/SDKClient_Windows")
 endif()
 
 # Header location is used as extraction marker so repeated configure runs are idempotent.
@@ -21,7 +21,7 @@ set(_manus_sdk_header_marker "${_manus_sdk_client_dir}/ManusSDK/include/ManusSDK
 file(MAKE_DIRECTORY "${_manus_sdk_base_dir}")
 
 if(NOT EXISTS "${_manus_sdk_header_marker}")
-  message(STATUS "Downloading Manus SDK ${_manus_sdk_version} from ${_manus_sdk_url}")
+  message(STATUS "Downloading Manus SDK ${MANUS_SDK_DOWNLOAD_VERSION} from ${_manus_sdk_url}")
   file(DOWNLOAD
     "${_manus_sdk_url}"
     "${_manus_sdk_zip_path}"
@@ -54,4 +54,5 @@ set(MANUS_ROOT_DIR
     CACHE PATH "Folder containing the ManusSDK"
     FORCE)
 
-message(STATUS "Using Manus SDK from ${MANUS_ROOT_DIR}")
+message(STATUS "Downloaded Manus SDK ${MANUS_SDK_DOWNLOAD_VERSION} to ${MANUS_ROOT_DIR}")
+message(STATUS "Using Manus SDK ${MANUS_SDK_DOWNLOAD_VERSION} from ${MANUS_ROOT_DIR}")
